eef6528c1ec26069d6f1bfd21102462ec4c0eaca
benitex-dev <ezequielpiki23@gmail.com>
2025-04-10 18:36:31 -0300

    Ignorar archivos temporales y de configuración

---
+++ TPWinForm_equipo-x/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TPWinForm_equipo_x
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblSaludar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(324, 248);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(130, 23);
+            this.btnSaludar.TabIndex = 0;
+            this.btnSaludar.Text = "saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // lblSaludar
+            // 
+            this.lblSaludar.AutoSize = true;
+            this.lblSaludar.Location = new System.Drawing.Point(321, 172);
+            this.lblSaludar.Name = "lblSaludar";
+            this.lblSaludar.Size = new System.Drawing.Size(35, 13);
+            this.lblSaludar.TabIndex = 1;
+            this.lblSaludar.Text = "label1";
+            this.lblSaludar.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSaludar);
+            this.Controls.Add(this.btnSaludar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label lblSaludar;
+    }
+}
+

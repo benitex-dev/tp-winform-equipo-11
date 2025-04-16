c2cb7b10646d0f489474de120f69ed859c909119
benitex-dev <benitezjorgedev@gmail.com>
2025-04-16 18:59:16 -0300

    Se actualiza nombre del proyecto a TPWinForm_equipo-11B

--- TPWinForm_equipo-x/Properties/AssemblyInfo.cs
+++ TPWinForm_equipo-x/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("TPWinForm_equipo-x")]
+[assembly: AssemblyTitle("TPWinForm_equipo-11B")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- TPWinForm_equipo-x/Properties/Resources.Designer.cs
+++ TPWinForm_equipo-x/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TPWinForm_equipo_11.Properties {
+namespace TPWinForm_equipo_11B.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TPWinForm_equipo_11.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TPWinForm_equipo_11.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TPWinForm_equipo_11B.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

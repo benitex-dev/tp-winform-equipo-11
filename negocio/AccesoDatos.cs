94aa9b37c61d6c25fc50bb4953f9d45c16026a88
benitex-dev <benitezjorgedev@gmail.com>
2025-04-16 19:16:53 -0300

    Se agregar atributos de clase AccesoDatos

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace negocio
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
     }
 }

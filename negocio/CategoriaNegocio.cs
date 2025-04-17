656ac146e723968ac1a6d808ead493469d5c1c37
Ivan Baigorria <ivann.baigorria@gmail.com>
2025-04-17 16:00:41 -0300

    Se agrega la Clase CategoriaNegocio, y el metodo listar en esta misma

---
+++ negocio/CategoriaNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> listaCategoria = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("SELECT ID, DESCRIPCION FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+
+                while (datos.Lector.Read())
+                {
+                    Categoria categoria = new Categoria();
+                    categoria.Id = (int)datos.Lector["Id"];
+                    categoria.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    listaCategoria.Add(categoria);
+
+                }
+
+                return listaCategoria;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+
+            }
+        }
+    }
+}

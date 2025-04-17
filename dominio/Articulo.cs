25a16641d6bc3ad775183090bc28edf6acba37ec
benitex-dev <benitezjorgedev@gmail.com>
2025-04-16 23:48:03 -0300

    Se agrega atributo descripcion a la clase Articulo, se crea la clase ArticuloNegocio con metodo listar() para probar lectura en la DB

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace dominio
         public int Id { get; set; }
         public string CodArticulo { get; set; }
         public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public float Precio { get; set; }
---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,44 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> articulos = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("SELECT ID,DESCRIPCION FROM ARTICULOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo articulo = new Articulo();
+                    articulo.Id = (int)datos.Lector["ID"];
+                    articulo.Descripcion = (string)datos.Lector["DESCRIPCION"];
+
+                    articulos.Add(articulo);
+                }
+
+                return articulos;
+            }
+            
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
